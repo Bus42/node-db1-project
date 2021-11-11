3ad64b5ccd29e4ca639d3b04944d3c2c53a48c55
Greg <gbrewton1980@gmail.com>
2021-11-10 22:46:22 -0700

    + dotenv dev dep, PORT to .env

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const server = require("./api/server.js");
+const colors = require("colors");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
-  console.log(`\n== API running on port ${PORT} ==\n`);
+  console.log(`\n== API running on port ${PORT} ==\n`.america);
 });
