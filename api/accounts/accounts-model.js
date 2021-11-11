c19c339147f8e4284e67ced9f469b4c4369a7cd4
Greg Brewton <gbrewton1980@gmail.com>
2021-11-10 22:02:00 -0700

    handles GET to '/' and '/:id'

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,38 @@
+const db = require('../../data/db-config');
+
+// resolves to an array of accounts (or an empty array)
 const getAll = () => {
-  // DO YOUR MAGIC
+  db('accounts')
+    .then(accounts => {
+      return accounts.length ? accounts : [];
+    })
 }
 
+// resolves to an account by the given id
 const getById = id => {
-  // DO YOUR MAGIC
+  db('accounts').where({ id })
+    .then(account => {
+      return account.length ? account[0] : null;
+    })
 }
 
-const create = account => {
-  // DO YOUR MAGIC
-}
+// // resolves to the newly created account
+// const create = account => {
+//   db.insert(account).into('accounts')
+//     .then(db('accounts').where({ id: account.id }).then(account => {
+//       return account.length ? account[0] : null;
+//     }))
+// }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
-}
+// // resolves to the updated account
+// const updateById = (id, account) => {
+//   // DO YOUR MAGIC
+// }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
-}
+// // resolves to the deleted account
+// const deleteById = id => {
+//   // DO YOUR MAGIC
+// }
 
 module.exports = {
   getAll,
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,39 @@
+const accountsModel = require('./accounts-model');
 const router = require('express').Router()
+router.use(express.json());
 
+// [GET] /api/accounts returns an array of accounts (or an empty array if there aren't any).
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  accountsModel.get().then(accounts => {
+    accounts ? res.status(200).send(accounts) : res.status(404).send({ message: 'Error fetching accounts' })
+  })
 })
 
+// [GET] /api/accounts/:id returns an account by the given id.
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+const id = req.params.id
+  accountsModel.getById(id).then(account => {
+    account ? res.status(200).send(account) : res.status(404).send({ message: 'Error fetching account' })
+  } )
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+// // [POST] /api/accounts returns the created account. Leading or trailing whitespace on budget name should be trimmed before saving to db.
+// router.post('/', (req, res, next) => {
+//   // DO YOUR MAGIC
+// })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-});
+// // [PUT] /api/accounts/:id returns the updated account. Leading or trailing whitespace on budget name should be trimmed before saving to db.
+// router.put('/:id', (req, res, next) => {
+//   // DO YOUR MAGIC
+// });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+// // [DELETE] /api/accounts/:id returns the deleted account.
+// router.delete('/:id', (req, res, next) => {
+//   // DO YOUR MAGIC
+// })
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+// router.use((err, req, res, next) => { // eslint-disable-line
+//   // DO YOUR MAGIC
+// })
 
 module.exports = router;
